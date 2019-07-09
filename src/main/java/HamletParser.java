59f0d55d5efcb98686eabe5a99eef10a6252a5c6
William Reyes <williamreyes814@yahoo.com>
2019-07-09 09:06:50 -0400

    done

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -32,7 +34,26 @@ public class HamletParser {
         return result.toString();
     }
 
+    public void replaceWithLeon(){     //make method that replace Hamlet with Leon
+        Pattern pattern = Pattern.compile("\\bHamlet|Hamlets\\b");// "Pattern" is utilizing regex to identify a specified pattern which is "Hamlet/Hamlets"
+        Matcher matcher = pattern.matcher(hamletData);// Matcher matcher is referencing the body of text that we want to search through which is "hamletData"
+        hamletData = matcher.replaceAll("Leon"); //matcher.replaceAll is replacing "pattern" (Hamlet|Hamlets) which is now "matcher" with "Leon"
+
+    }
+
+
+    public  void replaceWithTariq(){   //make method that replace Horatio with Tariq
+        Pattern pattern = Pattern.compile("\\bHoratio|Horatios\\b");
+        Matcher matcher = pattern.matcher(hamletData);
+        hamletData = matcher.replaceAll("Tariq");
+
+    }
+
+
+
     public String getHamletData(){
+
+
         return hamletData;
     }
 
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,10 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +20,49 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        HamletParser hamletParser = new HamletParser();
+        String hamletText = hamletParser.getHamletData();
+        String expected = hamletText.replaceAll("Hamlet", "Leon");
+
+        hamletParser.replaceWithLeon();
+
+        String actual = hamletParser.getHamletData();
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        HamletParser hamletParser = new HamletParser();
+        String hamletText = hamletParser.getHamletData();
+        String expected = hamletText.replaceAll("Horatio", "Tariq");
+
+        hamletParser.replaceWithTariq();
+
+        String actual = hamletParser.getHamletData();
+        Assert.assertEquals(expected, actual);
+
     }
 
-    @Test
-    public void testFindHoratio() {
-    }
+//    @Test
+//    public void testFindHoratio() {
+//
+//    }
+//
+//    @Test
+//    public void testFindHamlet() {
 
-    @Test
-    public void testFindHamlet() {
-    }
+//    }
+
+//    @Test
+//    public void testExample(){
+//        String myString = "William Leon Hamlet Tariq Horatio";
+//        Pattern pattern = Pattern.compile("Leon"); // What youre searching for in string
+//        Matcher matcher = pattern.matcher(myString);// where we want to search
+//
+//        String expected = "William Blue Hamlet Tariq Horatio";
+//        String actual = matcher.replaceAll("Blue");
+//
+//        Assert.assertEquals(expected, actual);
+
 }
